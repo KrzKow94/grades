4ce999b6114ee6cbc6d71712f15960a640fccaed
KrzKow94 <165143374+KrzKow94@users.noreply.github.com>
2024-05-27 12:10:07 +0200

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,17 @@
+def get_grade(score):
+    if score >= 90:
+        grade = print("bdb")
+    elif score >= 80:
+        grade = print("db+")
+    elif score >= 70:
+        grade = print("db")
+    elif score >= 60:
+        grade = print("dst+")
+    elif score >= 51:
+        grade = print("dst")
+    else:
+        grade = prin("ndst")
+    return grade
+
+get_grade(98)
+
